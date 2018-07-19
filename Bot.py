b95cde56eed8df025f67d9ba00831c536d22b78a
Loko44 <39665278+Loko44@users.noreply.github.com>
2018-07-19 17:18:35 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,124 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import random
+# -*- coding: UTF-8 -*-
+Client = discord.Client()
+client = commands.Bot(command_prefix = "/")
+botid="469477762017656842"
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('/napravigame') and message.author.id != botid:
+        Random=random.randint(0,26)
+        if Random==0:
+            msg=await client.send_message(message.channel, "Mapa: Amazonia\nhttp://aoe3.heavengames.com/pix/randommaps/rms_amazonia.gif")
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==1:
+            msg=await client.send_message(message.channel,'Mapa: Andes\nhttp://aoe3.heavengames.com/pix/randommaps/rms_andes.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==2:
+            msg=await client.send_message(message.channel,'Mapa: Araucania\nhttp://aoe3.heavengames.com/pix/randommaps/rms_araucania.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==3:
+            msg=await client.send_message(message.channel,'Mapa: Bayou\nhttp://aoe3.heavengames.com/pix/randommaps/rms_bayou.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==4:
+            msg=await client.send_message(message.channel,'Mapa: Borneo\nhttp://aoe3.heavengames.com/pix/randommaps/Borneo.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==5:
+            msg=await client.send_message(message.channel,'Mapa: California\nhttp://aoe3.heavengames.com/pix/randommaps/rms_california.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==6:
+            msg=await client.send_message(message.channel,'Mapa: Caribbean\nhttp://aoe3.heavengames.com/pix/randommaps/rms_caribbean.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==7:
+            msg=await client.send_message(message.channel,'Mapa: Carolina\nhttp://aoe3.heavengames.com/pix/randommaps/rms_carolina.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==8:
+            msg=await client.send_message(message.channel,'Mapa: Ceylon\nhttp://aoe3.heavengames.com/pix/randommaps/Ceylon.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==9:
+            msg=await client.send_message(message.channel,'Mapa: Deccan\nhttp://aoe3.heavengames.com/pix/randommaps/Deccan.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==10:
+            msg=await client.send_message(message.channel,'Mapa: Great Lakes\nhttp://aoe3.heavengames.com/pix/randommaps/rms_greatlakes.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==11:
+            msg=await client.send_message(message.channel,'Mapa: Great Plains\nhttp://aoe3.heavengames.com/pix/randommaps/rms_greatplains.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==12:
+            msg=await client.send_message(message.channel,'Mapa: Himalayas\nhttp://aoe3.heavengames.com/pix/randommaps/Himalayas.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==13:
+            msg=await client.send_message(message.channel,'Mapa: Himalayas - Upper\nhttp://aoe3.heavengames.com/pix/randommaps/Himalayasupper.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==14:
+            msg=await client.send_message(message.channel,'Mapa: New England\nhttp://aoe3.heavengames.com/pix/randommaps/rms_newengland.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==15:
+            msg=await client.send_message(message.channel,'Mapa: Northwest Territory\nhttp://aoe3.heavengames.com/pix/randommaps/rms_northwest.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==16:
+            msg=await client.send_message(message.channel,'Mapa: Orinoco\nhttp://aoe3.heavengames.com/pix/randommaps/rms_orinoco.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==17:
+            msg=await client.send_message(message.channel,'Mapa: Painted Desert\nhttp://aoe3.heavengames.com/pix/randommaps/rms_painteddesert.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==18:
+            msg=await client.send_message(message.channel,'Mapa: Pampas\nhttp://aoe3.heavengames.com/pix/randommaps/rms_pampas.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==19:
+            msg=await client.send_message(message.channel,'Mapa: Patagonia\nhttp://aoe3.heavengames.com/pix/randommaps/rms_patagonia.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==20:
+            msg=await client.send_message(message.channel,'Mapa: Rockies\nhttp://aoe3.heavengames.com/pix/randommaps/rms_rockies.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==21:
+            msg=await client.send_message(message.channel,'Mapa: Saguenay\nhttp://aoe3.heavengames.com/pix/randommaps/rms_saguenay.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==22:
+            msg=await client.send_message(message.channel,'Mapa: Sonora\nhttp://aoe3.heavengames.com/pix/randommaps/rms_sonora.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==23:
+            msg=await client.send_message(message.channel,'Mapa: Texas\nhttp://aoe3.heavengames.com/pix/randommaps/rms_texas.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==24:
+            msg=await client.send_message(message.channel,'Mapa: Unknown\nhttp://aoe3.heavengames.com/pix/randommaps/rms_unknown.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==25:
+            msg=await client.send_message(message.channel,'Mapa: Yucatan\nhttp://aoe3.heavengames.com/pix/randommaps/rms_yucatan.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==26:
+            msg=await client.send_message(message.channel,'Mapa: Yukon\nhttp://aoe3.heavengames.com/pix/randommaps/rms_yukon.gif')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+client.run("NDY5NDc3NzYyMDE3NjU2ODQy.DjIUDQ.JZfEQcYSGSnklF0y3mYvBJALLOw")
