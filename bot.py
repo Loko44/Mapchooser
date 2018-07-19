4d5398fe2a69a2e24fb1bef61eeaaefe5c24fd12
Loko44 <39665278+Loko44@users.noreply.github.com>
2018-07-19 21:20:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ botid="469477762017656842"
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('/napravigame') and message.author.id != botid:
+    if message.content.startswith('/napravigame') and message.author.id != botid and not message.content.contains('scenario') :
         Random=random.randint(0,30)
         if Random==0:
             msg=await client.send_message(message.channel, "Mapa: Amazonia\nhttp://aoe3.heavengames.com/pix/randommaps/rms_amazonia.gif")
@@ -135,6 +135,26 @@ async def on_message(message):
             await client.add_reaction(msg,"👍")
             await client.add_reaction(msg,"👎")
         elif Random==30:
+            msg=await client.send_message(message.channel,'Mapa: Azija\nhttps://cdn.discordapp.com/attachments/469228128024133635/469553724403220481/imageedit_5_9576557025.png')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+@client.event
+async def on_message(message):
+    if message.content.startswith('/napravigame') and message.content.contains('scenario') and  message.author.id != botid:
+        Random=random.randint(0,3)
+        if Random==0:
+            msg=await client.send_message(message.channel,'Mapa: Europa (small)\nhttps://cdn.discordapp.com/attachments/469228128024133635/469582325253013524/imageedit_9_4761086992.png')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==1:
+            msg=await client.send_message(message.channel,'Mapa: Europa (big)\nhttps://cdn.discordapp.com/attachments/469228128024133635/469579961292816384/imageedit_7_5916529584.png')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==2:
+            msg=await client.send_message(message.channel,'Mapa: Sj. Amerika\nhttps://cdn.discordapp.com/attachments/469228128024133635/469551739742453761/imageedit_1_3422115150.png')
+            await client.add_reaction(msg,"👍")
+            await client.add_reaction(msg,"👎")
+        elif Random==3:
             msg=await client.send_message(message.channel,'Mapa: Azija\nhttps://cdn.discordapp.com/attachments/469228128024133635/469553724403220481/imageedit_5_9576557025.png')
             await client.add_reaction(msg,"👍")
             await client.add_reaction(msg,"👎")
